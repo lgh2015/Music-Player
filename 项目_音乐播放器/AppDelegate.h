b03254958f90dca7406759ad36e3ee505bd95603
李国灏 <m13418019192_1@163.com>
2015-10-28 11:04:40 +0800

    Initial Commit

---
+++ 项目_音乐播放器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  项目_音乐播放器
+//
+//  Created by 李国灏 on 15/10/28.
+//  Copyright © 2015年 LiGuoHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 项目_音乐播放器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  项目_音乐播放器
+//
+//  Created by 李国灏 on 15/10/28.
+//  Copyright © 2015年 LiGuoHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
